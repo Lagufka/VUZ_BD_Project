7d9058791ca08f4cc836c0825f5d706ff2319317
Lagufka <xmat4@yandex.com>
2025-10-30 14:55:49 +0500

    progress database.py

--- database.py
+++ database.py
@@ -1,29 +1,42 @@
+from config import db_config
+
 import psycopg
 from psycopg import Connection, Cursor
-from psycopg.rows import dict_row, class_row
+from typing import Iterator
 from contextlib import contextmanager
-from typing import Iterator, Any, Optional
-
-import psycopg2.pool
-import psycopg_pool
-
-from config import DatabaseConfig, db_config
 
 
-class DatabaseManager(object):
-    def __init__(self, config: DatabaseConfig):
-        self.config = config
+class DatabaseManager:
+    def __init__(self) -> None:
+        self.config = db_config
 
-    
     def get_connection(self) -> Connection:
         return psycopg.connect(self.config.dsn)
-    
+
     @contextmanager
-    def get_cursor(self, row_factory=dict_row) -> Iterator[Cursor]:
+    def get_cursor(self) -> Iterator[Cursor]:
         conn = self.get_connection()
+
         try:
             with conn:
-                with conn.cursor(row_factory=row_factory) as cur:
+                with conn.cursor() as cur:
                     yield cur
         except Exception as e:
             raise e
+        finally:
+            conn.close()
+
+    @contextmanager
+    def transaction(self, arg):
+        conn = self.get_connection()
+
+        try:
+            with conn:
+                
+        except Exception as e:
+            raise e
+        finally:
+            conn.close()
+        
+    
+
