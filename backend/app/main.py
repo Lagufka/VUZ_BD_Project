797bcec8a8a77a566324c0f02ea9b4045b84f83a
Lagufka <xmat4@yandex.com>
2025-12-06 01:06:51 +0500

    add react building way

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,7 @@
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 from api.routers import products, buyers, sellers, parcels, points
-
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -13,7 +15,7 @@ app = FastAPI(
 # Настройка CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # В продакшене укажите конкретные домены
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -27,8 +29,36 @@ app.include_router(parcels.router)
 app.include_router(points.router)
 
 
+### Подгрузка статики ###
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+static_dir = os.path.join(current_dir, "static")
+
+app.mount("/static", StaticFiles(directory=static_dir), name="static")
+
+
+@app.get("/")
+async def serve_root():
+    return FileResponse(os.path.join(static_dir, "index.html"))
+
+
+@app.get("/{full_path:path}")
+async def serve_spa(full_path: str):
+    # Проверяем, существует ли файл
+    file_path = os.path.join(static_dir, full_path)
+
+    if os.path.isfile(file_path):
+        return FileResponse(file_path)
+
+    # Если файла нет - отдаём index.html (SPA роутинг)
+    return FileResponse(os.path.join(static_dir, "index.html"))
+
+
+### Дополнительные эндпоинты ###
+
+
 @app.get("/api/")
-async def root():
+async def api_root():
     return {
         "message": "Delivery System API",
         "version": "1.0.0",
